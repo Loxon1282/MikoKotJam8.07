629bc49003ef142bd0441e85d9a7123534627df7
Loxon1282 <loxon1282@gmail.com>
2018-07-08 20:56:13 +0200

    Start elements

---
+++ Assets/Scripts/HexagonScripts/Hexagon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour {
+
+
+	void Start () {
+		gameObject.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+			
+		}
+	
+	void Update () {
+		
+	}
+}
